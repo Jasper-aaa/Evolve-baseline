40391440c8e1268b3d7f52b7e3dbd9d53ee96b93
Jasper-aaa <yyifan519@gmail.com>
2025-09-07 09:28:09 +0300

    update script

--- scripts/bc_trans/finetune.sh
+++ scripts/bc_trans/finetune.sh
@@ -1,23 +1,27 @@
 
 # This script is used to finetune ResNet-T on downstream tasks
+for shot in 1 2 4 8
+do
+    python train.py --config-name=train_fewshot.yaml \
+        task=libero_long_fewshot \
+        task.demos_per_env=${shot} \
+        algo=bc_transformer \
+        exp_name=bctrans-libero-long-${shot}-shot \
+        variant_name=block_10 \
+        training.use_tqdm=true \
+        training.save_all_checkpoints=false \
+        training.use_amp=false \
+        train_dataloader.persistent_workers=true \
+        train_dataloader.num_workers=6 \
+        train_dataloader.batch_size=128 \
+        make_unique_experiment_dir=true \
+        training.auto_continue=false \
+        rollout.num_parallel_envs=5 \
+        rollout.rollouts_per_env=1 \
+        seed=0 \
+        logging.mode=disabled \
+        rollout.enabled=false \
 
-python train.py --config-name=train_fewshot.yaml \
-    task=libero_long_fewshot \
-    algo=bc_transformer \
-    exp_name=bctrans-libero-long-1shot \
-    variant_name=block_10 \
-    training.use_tqdm=true \
-    training.save_all_checkpoints=false \
-    training.use_amp=false \
-    train_dataloader.persistent_workers=true \
-    train_dataloader.num_workers=6 \
-    train_dataloader.batch_size=128 \
-    make_unique_experiment_dir=true \
-    training.auto_continue=false \
-    rollout.num_parallel_envs=5 \
-    rollout.rollouts_per_env=1 \
-    seed=0 \
-    logging.mode=disabled \
-
+done
 # Note1: training.auto_continue will automatically load the latest checkpoint from the previous training stage.
 #        Else you can specify the checkpoint_path to load a specific checkpoint.
--- scripts/quest/finetune.sh
+++ scripts/quest/finetune.sh
@@ -1,25 +1,30 @@
 
 # This script is used to train stage 2 i.e. finetuning Quest for downstream tasks
+for shot in 1 2 4 8
+do
+    python train.py --config-name=train_fewshot.yaml \
+        task=libero_long_fewshot \
+        task.demos_per_env=${shot} \
+        algo=quest \
+        exp_name=quest-libero-long-${shot}-shot \
+        variant_name=block_32_ds_4 \
+        training.use_tqdm=true \
+        training.save_all_checkpoints=false \
+        training.use_amp=false \
+        train_dataloader.persistent_workers=true \
+        train_dataloader.num_workers=6 \
+        make_unique_experiment_dir=false \
+        algo.skill_block_size=32 \
+        algo.downsample_factor=4 \
+        algo.l1_loss_scale=10 \
+        training.auto_continue=false \
+        rollout.num_parallel_envs=5 \
+        rollout.rollouts_per_env=5 \
+        seed=0 \
+        logging.mode=disabled \
+        rollout.enabled=false \
 
-python train.py --config-name=train_fewshot.yaml \
-    task=libero_long_fewshot \
-    algo=quest \
-    exp_name=quest-libero-long-1-shot\
-    variant_name=block_32_ds_4 \
-    training.use_tqdm=false \
-    training.save_all_checkpoints=false \
-    training.use_amp=false \
-    train_dataloader.persistent_workers=true \
-    train_dataloader.num_workers=6 \
-    make_unique_experiment_dir=false \
-    algo.skill_block_size=32 \
-    algo.downsample_factor=4 \
-    algo.l1_loss_scale=10 \
-    training.auto_continue=false \
-    rollout.num_parallel_envs=5 \
-    rollout.rollouts_per_env=5 \
-    seed=0 \
-    logging.mode=disabled \
+done
 
 # Note1: training.auto_continue will automatically load the latest checkpoint from the previous training stage.
 #        Else you can specify the checkpoint_path to load a specific checkpoint.
