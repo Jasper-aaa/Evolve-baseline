ead08088d45e71a3a6a13f6460f08ea8cbc3b77a
Jasper-aaa <yyifan519@gmail.com>
2025-09-07 14:42:40 +0300

    evaluate

---
+++ scripts/act/eval.sh
@@ -0,0 +1,15 @@
+task="$1"
+# This script is used to finetune ACT on downstream tasks
+for shot in 1 2 4 8
+do
+    python evaluate.py \
+        task=libero_${task}_fewshot \
+        algo=act \
+        exp_name=act-libero-${task}-${shot}-shot \
+        variant_name=block_16 \
+        algo.skill_block_size=16 \
+        training.use_tqdm=true \
+        seed=0 \
+
+# Note1: this will automatically load the latest checkpoint as per your exp_name, variant_name, algo, and stage.
+#        Else you can specify the checkpoint_path to load a specific checkpoint.
