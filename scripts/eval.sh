4b141835c796c9709f7a2bb0978e9ece62c41447
Jasper-aaa <yyifan519@gmail.com>
2025-09-07 13:52:09 +0300

    update eval

--- scripts/eval.sh
+++ scripts/eval.sh
@@ -1,14 +1,16 @@
+task="$1"
+for shot in 1 2 4 8
+do
+    python evaluate.py \
+        task=libero_${task} \
+        algo=diffusion_policy \
+        exp_name=dp-libero-${task}-${shot}-shot \
+        variant_name=block_32 \
+        stage=2 \
+        algo.skill_block_size=32 \
+        training.use_tqdm=true \
+        seed=0 \
 
-python evaluate.py \
-    task=libero_long \
-    algo=diffusion_policy \
-    exp_name=ft_dp_libero_long \
-    variant_name=block_32 \
-    stage=2 \
-    algo.skill_block_size=32 \
-    training.use_tqdm=true \
-    seed=0 \
-    checkpoint_path=/home/yeyifan/workplace/embodied/experiments/libero/LIBERO_10/diffusion_policy/libero_long_ft_fewshot/block_32/0/stage_2/multitask_model_epoch_0200.pth \
-
+done
 # Note1: this will automatically load the latest checkpoint as per your exp_name, variant_name, algo, and stage.
 #        Else you can specify the checkpoint_path to load a specific checkpoint.
